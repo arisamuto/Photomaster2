7360e86d6bc264b08d36fd68aa418773199835e0
arisamuto <cookiemonster.ca17@gmail.com>
2019-04-25 07:53:04 +0900

    Initial Commit

---
+++ Photomaster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photomaster
+//
+//  Created by Arisa Muto on 2019/04/25.
+//  Copyright © 2019 Arisa Muto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
